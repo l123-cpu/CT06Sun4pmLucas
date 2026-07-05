661adcd59e2790b0eeca08628c290c5e94e863e1
l123-cpu <lucasng978@gmail.com>
2026-07-05 09:16:53 +0000

    Jul 5, 2026, 9:16 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -1,11 +1,17 @@
 function setup(){
-    createCanvas(600, 400);
+    createCanvas(2000,2000);
     background(220);
 }
 
 function draw(){
     fill(67, 167, 69);
     ellipse(300,200,200,200);
-    
-    // Todo: Challenge 3 Traffic Light
 }
+function draw(){
+    fill(255, 255, 0);
+    ellipse(300,200,200,200);
+}
+function draw(){
+    fill(0, 167, 69);
+    ellipse(300,200,200,200);
+ }
