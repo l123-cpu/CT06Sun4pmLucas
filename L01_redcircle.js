9052767f7772bbf295f85f42886be4973d3a73aa
l123-cpu <lucasng978@gmail.com>
2026-07-05 09:19:08 +0000

    Jul 5, 2026, 9:19 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -6,14 +6,10 @@ function setup(){
 function draw(){
     fill(67, 167, 69);
     ellipse(300,200,200,200);
-}
-
-function draw(){
+  
     fill(255, 255, 0);
     ellipse(300,200,200,200);
-}
-
-function draw(){
-    fill(0, 167, 69);
+    
+    fill(255, 0, 0);
     ellipse(300,200,200,200);
 }
