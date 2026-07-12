19109c9931f51ce05aadd2b6b0dd6968159f9674
l123-cpu <lucasng978@gmail.com>
2026-07-12 08:25:42 +0000

    Jul 12, 2026, 8:25 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -3,9 +3,9 @@ function setup(){
 background(220);}
 function draw() {
   ellipse(200,400,100,100)
-  // Challenge 1: An eye usin
-  // g ellipse() function
+  // Challenge 1: An eye using ellipse() function
   ellipse(50,200,100,100)
+  ellipse
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
