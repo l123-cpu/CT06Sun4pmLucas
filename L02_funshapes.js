7746195ec9e964d428d37ed1ef36749c5be078c3
l123-cpu <lucasng978@gmail.com>
2026-07-12 08:42:08 +0000

    Jul 12, 2026, 8:42 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -12,6 +12,7 @@ function draw() {
   circle(800,80,100,100);
   circle(600,190,350,350);
   circle(500,150,50,50);
+  circle(500,150,50,50)
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
