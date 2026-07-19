a8a02fb4652770818fbda21b5a6ef19727f8dae2
l123-cpu <lucasng978@gmail.com>
2026-07-19 08:58:00 +0000

    Jul 19, 2026, 8:58 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -18,7 +18,7 @@ function draw() {
   // Task 2: Colour Loop
    fill(colourvalue)
    circle(100,100,100)
-   colourvalue +=25
+   colourvalue 
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
