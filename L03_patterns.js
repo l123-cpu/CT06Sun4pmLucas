d10262678f130feb294bef58ba5a807b91b4e78e
l123-cpu <lucasng978@gmail.com>
2026-07-19 08:33:36 +0000

    Jul 19, 2026, 8:33 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -11,7 +11,8 @@ function draw() {
   circle(375,200,100,100);
   circle(525,200,100,100)
   // Task 1: Colour Gradient
-
+    for(let i=0; i<5; i++)
+      circle
   // Task 2: Colour Loop
 
   // Task 3: Row of Circles
