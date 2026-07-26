ad53d7f1d11963d51ab849047021044c0ce804e6
l123-cpu <lucasng978@gmail.com>
2026-07-26 08:47:22 +0000

    Jul 26, 2026, 8:47 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -5,11 +5,8 @@ function setup(){
     background(220);
 
 }
-function draw(){
-   fill(sc)
-   circle (300,200,200)
-   
-   rect(10,20,20,20)}
+
+
 function mousePressed(){
     sc=colour(random(255), random(255), random(255))
 }
