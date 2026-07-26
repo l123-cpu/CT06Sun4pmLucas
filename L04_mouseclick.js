2353407a93f692571f7cdacbf17ae3536d2fd628
l123-cpu <lucasng978@gmail.com>
2026-07-26 08:33:19 +0000

    Jul 26, 2026, 8:33 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,10 +1,10 @@
 // write your codes here
 let sc= "blue";
 function setup(){
-createCanvas(600,400);
-background(220);
+    createCanvas(600,400);
+    background(220);
 
 }
 function draw(){
-   fill(shape)
+   fill(sc)
 }
