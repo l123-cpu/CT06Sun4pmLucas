4c20af108fa6183caa6e15acf23c3bba0d67a37d
l123-cpu <lucasng978@gmail.com>
2026-08-23 08:37:04 +0000

    Aug 23, 2026, 8:37 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,1 +1,22 @@
 // write your codes here
+let soundEffect, bgMusic,staticImage;
+function preload(){
+    soundEffect=loadSound('assets/pop.mp3');
+    bgMusic=loadSound('assets/bossaNova.mp3');
+    staticimage=loadImage('assets/pico-a.png');
+
+}
+function setup(){
+    createCanvas(400,400);
+    bgMusic.loop()
+}
+function setup(){
+    background("lightblue");
+    image(staticImage,0, 0, 50, 50);
+    
+}
+function KeyPressed(){
+    if(keycode === 32);{
+        soundEffect.play()
+    }
+}
