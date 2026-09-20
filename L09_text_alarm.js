2d8975677278b99237a0f1f3fa090e81d2295ccd
l123-cpu <lucasng978@gmail.com>
2026-09-20 08:26:16 +0000

    Sep 20, 2026, 8:26 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,18 +1,4 @@
 const { startTransition } = require("react");
 
 let x=200;
-let r=100;
-let g=100;
-let b=100;
-let speed=3;
-
-function setup(){
-    createCanvas(600,400)}
-
-function draw(){
-    background(220);
-    fill(r,g,b);
-    rect(x,200,40)
-    x+=speed
-    textsize(32)
-text("bounce!",0,200)}
+let speed=2
